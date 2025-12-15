983fcc5934e810f6b313c54b404da23aed15447a
svastik <svastiksharma13@gmail.com>
2025-12-15 12:52:56 -0500

    updated main.go to include the new routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"yuplan/internal/config"
+	"yuplan/internal/database"
+	"yuplan/internal/handlers"
+	"yuplan/internal/repository"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	ctx := context.Background()
+
+	cfg := config.Load()
+
+	// Setup db connection
+	pool, err := database.NewPool(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
+	courseRepo := repository.NewCourseRepository(pool)
+
+	courseHandler := handlers.NewCourseHandler(courseRepo)
+
+	// Setup router
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	api := r.Group("/api/v1")
+	{
+		api.GET("/courses", courseHandler.GetCourses)
+		api.GET("/courses/:course_id", courseHandler.GetCourseByID)
+	}
 
-	r.Run(":8080")
+	log.Printf("Starting server on port %s", cfg.Port)
+	if err := r.Run(":" + cfg.Port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
