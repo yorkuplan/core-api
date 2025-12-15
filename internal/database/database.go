8136ba4c2db2ead818ffb43371603be1cb642169
svastik <svastiksharma13@gmail.com>
2025-12-15 12:53:23 -0500

    added database.go to init. the db connection

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, databaseUrl string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(databaseUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse the database url: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("Unable to ping database: %w", err)
+	}
+
+	return pool, nil
+}
