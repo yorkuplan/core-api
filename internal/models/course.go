406b35fe0a2e62070ee33ee65cb260e3e8a45cc8
svastik <svastiksharma13@gmail.com>
2025-12-15 12:55:16 -0500

    defined course struct

--- internal/models/course.go
+++ internal/models/course.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+type Course struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Code        string    `json:"code"`
+	Credits     float64   `json:"credits"`
+	Description *string   `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
