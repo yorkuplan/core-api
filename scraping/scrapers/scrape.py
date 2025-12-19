113bbfa122dc006238d0b6a3bdafa93b837f248a
svastik <svastiksharma13@gmail.com>
2025-12-19 07:46:23 -0500

    added school of arts to automated scrape module

--- scraping/scrapers/scrape.py
+++ scraping/scrapers/scrape.py
@@ -13,6 +13,7 @@ try:
     import lassonde
     import urban
     import glendon
+    import school_of_arts
 except ImportError as e:
     print(f"Error importing scrapers: {e}")
     print("Make sure all scraper modules are available.")
@@ -68,6 +69,7 @@ def main():
         ("lassonde", lassonde, "Lassonde School of Engineering"),
         ("urban", urban, "Urban Studies"),
         ("glendon", glendon, "Glendon College"),
+        ("school_of_arts", school_of_arts, "School of Arts")
     ]
     
     results = []
