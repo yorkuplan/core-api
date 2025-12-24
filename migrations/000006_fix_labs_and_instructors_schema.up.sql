dc17863eaa2dbc5c023c98f5c5d079c02c5253ea
svastik <svastiksharma13@gmail.com>
2025-12-23 19:43:34 -0500

    made section_id's nullable since its possible a course is cancelled

--- migrations/000006_fix_labs_and_instructors_schema.up.sql
+++ migrations/000006_fix_labs_and_instructors_schema.up.sql
@@ -2,7 +2,7 @@
 ALTER TABLE labs DROP CONSTRAINT IF EXISTS labs_course_id_fkey;
 DROP INDEX IF EXISTS idx_labs_course;
 ALTER TABLE labs DROP COLUMN IF EXISTS course_id;
-ALTER TABLE labs ADD COLUMN section_id UUID NOT NULL REFERENCES sections(id) ON DELETE CASCADE;
+ALTER TABLE labs ADD COLUMN section_id UUID REFERENCES sections(id) ON DELETE CASCADE;
 CREATE INDEX idx_labs_section ON labs(section_id);
 
 -- Alter instructors table: add section_id
